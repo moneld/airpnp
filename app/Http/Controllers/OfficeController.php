c09d93818ac83a6fd0ff2a80540ba3a50aa9df68
moneld <45119291+moneld@users.noreply.github.com>
2022-09-07 18:22:55 +0100

    filtre

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -4,17 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OfficeResource;
 use App\Models\Office;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class OfficeController extends Controller
 {
     //
 
+
     public function index() : \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
         $offices = Office::query()
-            ->latest('id')
-            ->get();
+            ->where('approval_status',Office::APPROVAL_APPROVED)
+            ->where('hidden',false)
+            ->when(request('host_id'), fn($builder) => $builder->whereUserId(request('host_id')))
+            ->when(request('user_id'), fn($builder) => $builder->whereRelation('reservations', 'user_id','=',request('user_id')))
+            ->when(
+                request('lat') && request('lng'),
+                fn($builder) => $builder->nearestTo(request('lat'),request('lng')),
+                fn($builder) => $builder->orderBy('id','ASC')
+            )
+            ->with(['images','tags','user'])
+            ->withCount(['reservations' => fn($builder) => $builder->where('status',Reservation::STATUS_ACTIVE)])
+            ->paginate(20);
 
         return OfficeResource::collection(
             $offices
--- tests/Feature/OfficesControllerTest.php
+++ tests/Feature/OfficesControllerTest.php
@@ -2,25 +2,154 @@
 
 namespace Tests\Feature;
 
+use App\Models\Image;
 use App\Models\Office;
+use App\Models\Reservation;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class OfficesControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * @test
      */
-    public function getAllOffices()
+    public function itListsAllOfficesInPaginatedWay()
     {
-        $offices = Office::factory(3)->create();
+       Office::factory(3)->create();
 
         $response = $this->get('/api/offices');
 
         $response->assertOk();
 
-        $response->assertJsonCount(15,'data');
+        $response->assertJsonCount(3,'data');
+
+        $this->assertNotNull($response->json('meta'));
+        $this->assertNotNull($response->json('links'));
     }
 
+    /**
+    *@test
+     */
+    public function itOnlyListsOffciesThatAreNotHiddenAndApproved()
+    {
+        Office::factory(3)->create();
+        Office::factory()->create(['hidden' => true]);
+        Office::factory()->create(['approval_status' => Office::APPROVAL_PENDING]);
+
+        $response = $this->get('/api/offices');
+
+        $response->assertOk();
+
+        $response->assertJsonCount(3,'data');
+    }
+
+    /**
+     *@test
+     */
+    public function itFilterByHostId()
+    {
+        Office::factory(3)->create();
+       $host = User::factory()->create();
+
+       Office::factory()->for($host)->create();
+
+        $response = $this->get('/api/offices?host_id='.$host->id);
+
+        $response->assertOk();
+
+        $response->assertJsonCount(1,'data');
+    }
+
+    /**
+     *@test
+     */
+    public function itFilterByUserId()
+    {
+
+        $user = User::factory()->create();
+
+        $office = Office::factory()->create();
+
+        Reservation::factory()->for($office)->for($user)->create();
+
+        $response = $this->get('/api/offices?user_id='.$user->id);
+
+        $response->assertOk();
+
+        $response->assertJsonCount(1,'data');
+    }
+
+
+    /**
+     * @test
+     */
+    public function itIncludeImagesTagsUser()
+    {
+
+       $tag = Tag::factory()->create();
+       $user = User::factory()->create();
+       $office = Office::factory()->for($user)->create();
+
+        $office->tags()->attach($tag);
+        $office->images()->create(['path'=>'image.jpg']);
+
+        $response = $this->get('/api/offices');
+        $response->assertOk();
+
+        $this->assertIsArray($response->json('data')[0]['tags']);
+        $this->assertIsArray($response->json('data')[0]['images']);
+        $this->assertEquals($response->json('data')[0]['user']['id'],$user->id);
+
+    }
+
+
+    /**
+     * @test
+     */
+    public function itReturnsTheNumberOfActiveReservations()
+    {
+        $office = Office::factory()->create();
+        Reservation::factory()->for($office)->create(['status' => Reservation::STATUS_ACTIVE]);
+        Reservation::factory()->for($office)->create(['status' => Reservation::STATUS_CANCELLED]);
+
+        $response = $this->get('/api/offices');
+        $response->assertOk();
+        $this->assertEquals($response->json('data')[0]['reservations_count'],1);
+
+    }
+
+    /**
+     * @test
+     */
+    public function itOrdersByDistanceWhenCoordinatesAreProvided ()
+    {
+
+        /*
+         * Cotonou
+         * lat = 7.934327726169804
+         * lng = 1.975135952890811
+         * */
+        $office = Office::factory()->create(
+            [
+                'lat' => '6.370246273189285',
+                'lng' => '2.3930874928228523',
+                'title' => 'Cotonou Office'
+            ]
+        );
+
+        $office2 = Office::factory()->create(
+            [
+                'lat' => '9.329142401738267',
+                'lng' => '2.633971881784387',
+                'title' => 'Parakou Office'
+            ]
+        );
+
+        $response = $this->get('/api/offices?lat=7.934327726169804&lng=1.975135952890811');
+        $response->dump();
+    }
 }
